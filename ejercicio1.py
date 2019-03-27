b3edfda27a30f6129ae27cd37b60253283e27acd
Jose Maria Montero <josema9817@gmail.com>
2019-03-27 14:40:09 +0100

    Terminado

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,11 +1,9 @@
 from lxml import etree
 doc = etree.parse('alquilercoches.xml')
 
-indicador = False
-
 def buscar(telefono,doc):
 	telefonos = doc.xpath('//telefono[content="%s"]/../nombre/content/text()' % telefono) 
-	indicador = True
+	print(telefonos)
 	return telefonos
 
 telefono = input("Introduce un numero de telefono: ")
